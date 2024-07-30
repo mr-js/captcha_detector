0490bf9a13f0b0edb23d7750816a3b4dbfb1bf15
mr-js <mr-js@bk.ru>
2024-07-30 22:14:38 +0300

    RC 1.0
    + Cloudflare Common Type (Demo)
    + Proxy support

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # captcha_detector
  Detect the captcha type right away
 
+ ## Features
+ - Cloudflare common type support (demo)
+ - Last dump saving
+ - Proxy support
+
  ## Example
 
  ```python
  url = 'https://4pda.to'
- cd = CaptchaDetector()
+ cd = CaptchaDetector(proxies={'https':'socks5://127.0.0.1:2080'})
  pt = cd.explore(url)
  ```
 
--- captcha_detector/captcha_detector.py
+++ captcha_detector/captcha_detector.py
@@ -1,16 +1,47 @@
+import requests
+import codecs
+import lxml
+
+
 class CaptchaDetector:
     ...
     
-    def __init__(self) -> None:
+    def __init__(self, proxies={}) -> None:
+        self.proxies = proxies
         pass
 
     def explore(self, url) -> str:
-        protect_type = 'Cloudflare.Turnstile'
-        ...
-        return protect_type
+        protect_type = ''
+        if self.proxies:
+            response = requests.get(url, proxies=self.proxies)
+        else:
+            response = requests.get(url)
+        if response.status_code == 200 or 403:
+            ...
+            content = response.text
+            dump_file = 'dump.html'
+            with codecs.open(dump_file, 'w', 'utf-8') as f:
+                f.write(content)
+            print(f'Downloaded {url} finished ({round(len(content)/1024)} KB received, status code {response.status_code}, last dump in "{dump_file}")')
+            if 'captcha' in content:
+                # common type "cloudflare"
+                if 'cf-please-wait' in content:
+                    protect_type = 'cloudflare'
+                    ...
+                    # cloudflare subtype refinement
+                    if ...:
+                        ...
+                        protect_type += '.turnstile'
+                    else:
+                        ...
+                return protect_type
+        else:
+            pass
+
+        return 'Unknown'        
 
 
+cd = CaptchaDetector(proxies={'https':'socks5://127.0.0.1:2080'})
 url = 'https://4pda.to'
-cd = CaptchaDetector()
 pt = cd.explore(url)
 print(f'Detected protection type on {url}: {pt}')
